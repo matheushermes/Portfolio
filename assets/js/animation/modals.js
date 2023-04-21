7312372fd2fc9c8018e1559803be878c25ec12b0
Matheus Hermes <matheus.hermes97@gmail.com>
2023-04-21 19:39:24 -0300

    Adding modal of contact

--- assets/js/animation/modals.js
+++ assets/js/animation/modals.js
@@ -1,19 +1,41 @@
+let pageOverlay = document.getElementById("page-overlay");
+
 //Modal de SKILLS;
 let openModalSkill = document.querySelector(".open-modal-skill");
 let btnOpenModalSkill = document.querySelector(".btn-open-modal-skill");
 let closeModalSkill = document.getElementById("close-skill");
-let modalSkill =  document.getElementById("modal-skills");
-let pageOverlay = document.getElementById("page-overlay");
+let modalSkill = document.getElementById("modal-skills");
 
-function openModal() {
+
+function openModalSkills() {
     modalSkill.classList.add('active-modal-skill');
     pageOverlay.classList.add('active-page-overlay');
 }
 
-openModalSkill.addEventListener('click', openModal);
-btnOpenModalSkill.addEventListener('click', openModal);
+openModalSkill.addEventListener('click', openModalSkills);
+btnOpenModalSkill.addEventListener('click', openModalSkills);
 
 closeModalSkill.addEventListener('click', () => {
     modalSkill.classList.remove('active-modal-skill');
+    pageOverlay.classList.remove('active-page-overlay');
+});
+
+
+//Modal de CONTACT;
+let openModalContact = document.querySelector(".open-modal-contact");
+let btnOpenModalContact = document.querySelector(".btn-open-modal-contact");
+let closeModalContact = document.getElementById("close-contact");
+let modalContact = document.getElementById("modal-contact");
+
+function openModalContacts() {
+    modalContact.classList.add('active-modal-contact');
+    pageOverlay.classList.add('active-page-overlay');
+}
+
+openModalContact.addEventListener('click', openModalContacts);
+btnOpenModalContact.addEventListener('click', openModalContacts);
+
+closeModalContact.addEventListener('click', () => {
+    modalContact.classList.remove('active-modal-contact');
     pageOverlay.classList.remove('active-page-overlay');
 });
