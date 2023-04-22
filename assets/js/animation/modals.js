967593deeeda2455c4796c37ff2e174013f37529
Matheus Hermes <matheus.hermes97@gmail.com>
2023-04-21 22:13:46 -0300

    adding modal of projects

--- assets/js/animation/modals.js
+++ assets/js/animation/modals.js
@@ -1,6 +1,6 @@
 let pageOverlay = document.getElementById("page-overlay");
 
-//Modal de SKILLS;
+//Modal de habilidades;
 let openModalSkill = document.querySelector(".open-modal-skill");
 let btnOpenModalSkill = document.querySelector(".btn-open-modal-skill");
 let closeModalSkill = document.getElementById("close-skill");
@@ -21,7 +21,7 @@ closeModalSkill.addEventListener('click', () => {
 });
 
 
-//Modal de CONTACT;
+//Modal de contato;
 let openModalContact = document.querySelector(".open-modal-contact");
 let btnOpenModalContact = document.querySelector(".btn-open-modal-contact");
 let closeModalContact = document.getElementById("close-contact");
@@ -37,5 +37,24 @@ btnOpenModalContact.addEventListener('click', openModalContacts);
 
 closeModalContact.addEventListener('click', () => {
     modalContact.classList.remove('active-modal-contact');
+    pageOverlay.classList.remove('active-page-overlay');
+});
+
+//Modal de projetos;
+let openModalProject = document.querySelector(".open-modal-project");
+let btnOpenModalProject = document.querySelector(".btn-open-modal-project");
+let closeModalProject = document.getElementById("close-project");
+let modalProject = document.getElementById("modal-projects");
+
+function openModalProjects() {
+    modalProject.classList.add('active-modal-project');
+    pageOverlay.classList.add('active-page-overlay');
+}
+
+openModalProject.addEventListener('click', openModalProjects);
+btnOpenModalProject.addEventListener('click', openModalProjects);
+
+closeModalProject.addEventListener('click', () => {
+    modalProject.classList.remove('active-modal-project');
     pageOverlay.classList.remove('active-page-overlay');
 });
